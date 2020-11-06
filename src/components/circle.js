7c9056153031d723f00ec4e17dcbb88e87cbe019
HariKishore7 <harikishoret99@gmail.com>
2020-11-06 15:24:05 +0530

    Constructors added

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
-
-
+const Shape=require('./shape');
+class Circle extends Shape{
+    constructor(){
+        super();
+    }
+    calculateArea(){
+        console.log("Area of circle color is: "+this.color);
+        return 3.14*5*5;
+    }
+}
+module.exports=Circle;
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+    constructor(){
+        this.color="green";
+    }
+    drawShape(){
+        console.log("Drwaing");
+    }
+    calculateArea(){
+        console.log("No Areas provided");
+    }
+}
+module.exports=Shape;
